fe124ae33e1fcf27c1ff9ba4e05e4dfec86ee031
francacj <francacj@gmail.com>
2026-02-22 19:55:11 +0100

    Add files via upload

---
+++ install_packages.R
@@ -0,0 +1,21 @@
+# Install packages without renv
+
+# pacman 
+if (!requireNamespace("pacman", quietly = TRUE)) {
+  install.packages("pacman")
+}
+
+pacman::p_load(
+  shiny,
+  DT,
+  shinyjs,
+  officer,
+  flextable,
+  readr,
+  dplyr,
+  writexl,
+  magick,
+  lubridate,
+  stringr
+)
+
